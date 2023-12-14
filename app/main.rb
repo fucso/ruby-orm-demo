30712e604bab7f472c3fef14aba438db5c371833
sohosoki <so.hosoki@enechange.co.jp>
2023-12-14 20:23:56 +0900

    find by capa

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,19 @@
 require_relative 'repositories/sequel/plan_repository.rb'
 
-# PlanRepositoryを使ってPlanの一覧を取得
-plans = Repository::Sequel::PlanRepository.all
-puts "all: #{plans.size} plans found."
+capacity_cases = [10, 20, 30, 40, 50, 60, 61, 490, 500]
+def count_time(times: 1)
+  start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  times.times { yield if block_given? } 
+  end_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  execution_time = end_time - start_time
+  execution_time # 返り値として実行時間を秒単位で返す
+end
+
+# sequel
+time = count_time(times: 100) do
+  plans = Repository::Sequel::PlanRepository.all
+  capacity_cases.each do |capacity|
+    plans = Repository::Sequel::PlanRepository.find_by_capacity(capacity)
+  end
+end
+puts "sequel: #{time} sec"
--- app/repositories/sequel/plan_repository.rb
+++ app/repositories/sequel/plan_repository.rb
@@ -4,6 +4,7 @@ require_relative './demand_charge_repository.rb'
 require_relative './energy_charge_repository.rb'
 
 DB = Sequel.connect('postgres://postgres:password@db:5432/mydatabase')
+QUERY = DB[Sequel[:plans]]
 BUILDERS = {
   demand_charges: Repository::Sequel::DemandChargeRepository,
   energy_charges: Repository::Sequel::EnergyChargeRepository
@@ -15,20 +16,40 @@ module Repository
 
       class << self
         def all()
-          plans = DB[:plans].all
-          plan_ids = plans.map { |p| p[:id] }
+          dataset_to_model(QUERY.all)
+        end
+
+        def find_by_capacity(capacity)
+          dataset = QUERY.join(:demand_charges, plan_id: :id).select_all(:plans).where(::Sequel.lit('? BETWEEN ampere_from AND ampere_to', capacity))
+          dataset_to_model(dataset.all)
+        end
+
+        def to_model(record, demand_charges: [], energy_charges: [])
+          Plan.new(
+            id: record[:id],
+            name: record[:name],
+            code: record[:code],
+            demand_charges: demand_charges,
+            energy_charges: energy_charges
+          )
+        end
+
+        private
+
+        def dataset_to_model(dataset)
+          plan_ids = dataset.map { |p| p[:id] }
           relations = BUILDERS.keys.map do |table|
             records = DB[table].where(plan_id: plan_ids).all
             [table, records.group_by { |r| r[:plan_id] }]
           end.to_h
           
-          plans.map do |plan|
+          dataset.map do |plan|
             id = plan[:id]
             sub_models = relations.map do |table, record_by_plan|
               rows = record_by_plan[id]
               [table, rows.map { |row| BUILDERS[table].to_model(row) }]
             end.to_h
-            Plan.new(**(plan.merge(sub_models)))
+            to_model(plan, **sub_models)
           end
         end
       end
